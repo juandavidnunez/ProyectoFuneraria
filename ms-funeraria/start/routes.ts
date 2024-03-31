3ca580e7a1ba08301150b27afce41b84601b8a26
BGHcar <bryan.1701720431@ucaldas.edu.co>
2024-03-30 23:15:29 -0500

    bryan

--- ms-funeraria/start/routes.ts
+++ ms-funeraria/start/routes.ts
@@ -1,34 +1,25 @@
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| This file is dedicated for defining HTTP routes. A single file is enough
+| for majority of projects, however you can define routes in different
+| files and just make sure to import them inside this file. For example
+|
+| Define routes in following two files
+| ├── start/routes/cart.ts
+| ├── start/routes/customer.ts
+|
+| and then import them inside `start/routes.ts` as follows
+|
+| import './routes/cart'
+| import './routes/customer'
+|
+*/
+
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.group(() => {
-    Route.get("/usuarios", "UsuariosController.index");
-    Route.get("/usuarios/:id", "UsuariosController.show");
-    Route.post("/usuarios", "UsuariosController.store");
-    Route.put("/usuarios/:id", "UsuariosController.update");
-    Route.delete("/usuarios/:id", "UsuariosController.destroy");
+Route.get('/', async () => {
+  return { hello: 'world' }
 })
-
-Route.group(() => {
-    Route.get("/roles", "RolesController.index");
-    Route.get("/roles/:id", "RolesController.show");
-    Route.post("/roles", "RolesController.store");
-    Route.put("/roles/:id", "RolesController.update");
-    Route.delete("/roles/:id", "RolesController.destroy");
-})
-
-Route.group(() => {
-    Route.get("/permisos", "PermisosController.index");
-    Route.get("/permisos/:id", "PermisosController.show");
-    Route.post("/permisos", "PermisosController.store");
-    Route.put("/permisos/:id", "PermisosController.update");
-    Route.delete("/permisos/:id", "PermisosController.destroy");
-})
-
-Route.group(() => {
-    Route.get("/rolespermisos", "RolesPermisosController.index");
-    Route.get("/rolespermisos/:id", "RolesPermisosController.show");
-    Route.post("/rolespermisos", "RolesPermisosController.store");
-    Route.put("/rolespermisos/:id", "RolesPermisosController.update");
-    Route.delete("/rolespermisos/:id", "RolesPermisosController.destroy");
-})
-
